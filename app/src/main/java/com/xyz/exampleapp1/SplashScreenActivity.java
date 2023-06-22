caf05e67345813cbb9613a8afcea89c874b3f0cf
leenimad52 <leenimad52@gmail.com>
2023-06-22 09:59:05 +0300

    splashscreen

---
+++ app/src/main/java/com/xyz/exampleapp1/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.xyz.exampleapp1;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Animation textAnimation = AnimationUtils.loadAnimation(this, R.anim.text_animation);
+        Animation logoAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+
+        TextView textViewTitle = findViewById(R.id.splash_title);
+        ImageView imageViewLogo = findViewById(R.id.spash_logo);
+
+        textViewTitle.setAnimation(textAnimation);
+        imageViewLogo.setAnimation(logoAnimation);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
